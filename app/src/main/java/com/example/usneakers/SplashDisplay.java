e882a8e835220fb15dd86adead184fbee6362457
Leslie <lesliezhu91@gmail.com>
2020-05-12 11:59:52 +1000

    UI Develop 1 - adding loading screen

---
+++ app/src/main/java/com/example/usneakers/SplashDisplay.java
@@ -0,0 +1,39 @@
+package com.example.usneakers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActionBar;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.WindowManager;
+
+public class SplashDisplay extends AppCompatActivity {
+
+    //Define the duration of waiting time for 3s
+    private final int SPLASH_DISPLAY_LENGTH = 3000;
+
+    //when activity is firstly created
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        /*Remove the status bar for just loading activity*/
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash_display);
+
+        /*New Handler to start the activity
+        * and close this Splash Display activity with the duration defined before*/
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                Intent mainIntent = new Intent(SplashDisplay.this, MainActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
