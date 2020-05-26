aab422bee1e01c9ec6e2abaffcf967b329a66fb9
Leslie <lesliezhu91@gmail.com>
2020-05-26 18:17:14 +1000

    Revert "Development - Implement Google map API in verification page"
    This reverts commit 859497ec

--- app/src/main/java/com/example/usneakers/home/Gallery.java
+++ app/src/main/java/com/example/usneakers/home/Gallery.java
@@ -11,13 +11,9 @@ import com.example.usneakers.R;
 
 public class Gallery extends Fragment {
 
-    View view;
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.fragment_gallery, container, false);
-
-        return view;
+        return inflater.inflate(R.layout.fragment_gallery, container, false);
     }
 }
